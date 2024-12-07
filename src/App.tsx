e8e1ddc0aeb7a103dea73ed64a85e1f023788d69
swaminaing <smnaing9.smn@gmail.com>
2024-12-07 20:44:37 +0630

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import CourseGoal from './components/CourseGoal'
+
+function App() {
+
+  return (
+    <main>
+      <CourseGoal />
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/CourseGoal.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const CourseGoal = () => {
+  return (
+    <article>
+        <div>
+            <h2>Title</h2>
+            <p>Description</p>
+        </div>
+        <button>Delete</button>
+    </article>
+  )
+}
+
+export default CourseGoal
