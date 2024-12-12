6724b651461f00777792fc78464f6285241fff84
swaminaing <smnaing9.smn@gmail.com>
2024-12-12 21:31:53 +0630

    create InfoBox component to show information based on specific conditions

--- src/components/CourseGoalList.tsx
+++ src/components/CourseGoalList.tsx
@@ -1,27 +1,36 @@
 import CourseGoal from "./CourseGoal";
-import {type CourseGoal as CGoal} from "../App"
+import { type CourseGoal as CGoal } from "../App";
+import InfoBox from "./InfoBox";
+import { type ReactNode } from "react";
 
 type CourseGoalListProps = {
-    goals: CGoal[];
-    onDeleteGoal: (id: number) => void;
-}
+  goals: CGoal[];
+  onDeleteGoal: (id: number) => void;
+};
 
-const CourseGoalList = ({goals, onDeleteGoal}: CourseGoalListProps) => {
+const CourseGoalList = ({ goals, onDeleteGoal }: CourseGoalListProps) => {
+  if (goals.length === 0) {
+    return <InfoBox mode="hint">You have no goals. Let's create some</InfoBox>;
+  }
+
+  let warningBox: ReactNode;
+  warningBox = goals.length >= 4 ? <InfoBox mode="warning">You submit probably too much goals.</InfoBox> : null;
   return (
+    <>
+    {warningBox}
     <ul>
-        {
-            goals.map(goal => {
-                return(
-                    <li key={goal.id}>
-                        <CourseGoal id={goal.id} title={goal.title} onDelete={onDeleteGoal}>
-                            <p>{goal.description}</p>
-                        </CourseGoal>
-                    </li>
-                )
-            })
-        }
+      {goals.map((goal) => {
+        return (
+          <li key={goal.id}>
+            <CourseGoal id={goal.id} title={goal.title} onDelete={onDeleteGoal}>
+              <p>{goal.description}</p>
+            </CourseGoal>
+          </li>
+        );
+      })}
     </ul>
-  )
-}
+    </>
+  );
+};
 
-export default CourseGoalList
+export default CourseGoalList;
